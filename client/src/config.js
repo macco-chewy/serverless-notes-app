af9bff0b48f49e4ca26a45db028552fbc99daa81
unklcid <mike.a.cherry@gmail.com>
2018-01-14 20:32:57 -0800

    Added code (#1)

---
+++ client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  cognito: {
+    USER_POOL_ID: "us-west-2_Mc1TRJpRC",
+    APP_CLIENT_ID: "721q6surl0b38a3geaehunf753",
+    REGION: "us-west-2",
+    IDENTITY_POOL_ID: "us-west-2:6283cf92-b5ff-427e-b8f5-57fff1f32ff2",
+  },
+  apiGateway: {
+    URL: "https://a9v51wy31g.execute-api.us-west-2.amazonaws.com/prod",
+    REGION: "us-west-2",
+  },
+  s3: {
+    BUCKET: "zs-notes-app-uploads",
+  },
+};
